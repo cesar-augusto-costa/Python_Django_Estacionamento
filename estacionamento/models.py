5f622360dc39d6903e2fa76feb399171b2c46e95
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2024-07-27 15:40:00 -0300

    Criando o modelo

--- estacionamento/models.py
+++ estacionamento/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Estacionamento(models.Model):
+    nomeCliente = models.CharField(max_length=150, null=False, blank=False)
+    nomeCarro = models.CharField(max_length=150, null=False, blank=False)
+    placa = models.CharField(max_length=150, null=False, blank=False)
+    dataEntrada = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    dataSaida = models.DateTimeField(null=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2, null=True)
