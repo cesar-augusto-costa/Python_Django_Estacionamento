f4f98c33b5bbfc74a752c6710e5f247f4b1a9f66
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2024-07-27 14:08:13 -0300

    Template Home

--- estacionamento/views.py
+++ estacionamento/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('view Home')
+    # return HttpResponse('view Home')
+    return render(request,'estacionamento/home.html')
